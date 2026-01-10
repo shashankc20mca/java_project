de87f8cf9b5808924bbe6013d0ccdc7723970ec4
shashank c <120564691+shashankc20mca@users.noreply.github.com>
2026-01-10 11:57:37 +0530

    Update myfirst.java

--- myfirst.java
+++ myfirst.java
@@ -2,6 +2,8 @@ public class myfirst {
         public static void main(String[] args) {
                 System.out.println("Heloo from java program");
                 System.out.println("Heloo from java22222222 program");
+this is error
+
 
         }
 }
